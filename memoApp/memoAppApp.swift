09addcaf57ceddc1e12a6b2016f4480305b708c9
k-yutok <ciyt11058@g.nihon-u.ac.jp>
2023-07-01 19:43:26 +0900

    Initial Commit

---
+++ memoApp/memoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoAppApp.swift
+//  memoApp
+//
+//  Created by yuutoku kouno on 2023/07/01.
+//
+
+import SwiftUI
+
+@main
+struct memoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
