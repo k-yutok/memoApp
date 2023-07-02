55c86ce829f4b149e7b938cb5951defa4f62ceff
k-yutok <ciyt11058@g.nihon-u.ac.jp>
2023-07-02 20:23:40 +0900

    メモ一覧となるリストのモックを作成。

--- memoApp/ContentView.swift
+++ memoApp/ContentView.swift
@@ -1,23 +1,56 @@
-//
-//  ContentView.swift
-//  memoApp
-//
-//  Created by yuutoku kouno on 2023/07/01.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var memolist = ["今日のメモ","Todo"]
+    @State private var  memolistSub = ["aa","bb","cc"]
+ //   @State var editMode: Editmode = .inactive
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        // 参考: https://capibara1969.com/2266/
+        
+        NavigationView{
+            // ForEachを用いた動的リスト
+            List{
+                Section {
+                    
+                    ForEach(0 ..< memolist.count) { index in
+                        Text(memolist[index])
+                    }
+                    .onDelete(perform: rowRemove)
+                } header: {
+                    Text("TODO")
+                }
+                
+                // Section
+                Section {
+                    
+                    Text("aaa")
+                }header: {
+                    Text("memo")
+                }
+                //   Spacer(minLength: 20) // 空白 size:minLength
+            }
+            // Editボタン
+            .toolbar{
+              EditButton()
+            }
+            .listStyle(.insetGrouped)
+
+            
+            // 削除はできなくなるがこの書き方もできる
+            /*
+             List(memolistSub, id: \.self){ memo in
+             Text(memo)
+             }
+             .listStyle(.plain) // Listのスタイル何も指定しない場合はDefaultListStyle()
+             */
+            
         }
-        //
-        //
-        .padding()
+    }
+    
+    func rowRemove(offsets: IndexSet) {
+        memolist.remove(atOffsets: offsets)
     }
 }
 
